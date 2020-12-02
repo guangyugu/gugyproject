6444d07c636f9a9947bec1373e1d96df90b283a4
gugy <839793849@qq.com>
2020-12-02 09:48:11 +0800

    init

---
+++ src/main/java/org/gugy/gugyproject/GugyprojectApplication.java
@@ -0,0 +1,13 @@
+package org.gugy.gugyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GugyprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GugyprojectApplication.class, args);
+    }
+
+}
